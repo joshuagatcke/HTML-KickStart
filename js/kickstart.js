5b25aa6abf9f6a6b6c77424f4e75310890f3929a
joshua gatcke <joshua@99lime.com>
2013-02-08 13:24:15 -0700

    Example Docs Cleanup

--- js/kickstart.js
+++ js/kickstart.js
@@ -285,6 +285,7 @@ jQuery(document).ready(function($){
 	$('ul').find('li:first-child').addClass('first');
 	$('ul').find('li:last-child').addClass('last');
 	$('hr').before('<div class="clear">&nbsp;</div>');
+	$('[class*="col_"]').addClass('column');
 	$('pre').addClass('prettyprint');prettyPrint();
 	
 });
